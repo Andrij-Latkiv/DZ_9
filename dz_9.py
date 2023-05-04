7f3069b36111a9a78ca9f224ba57fe3f8caeaeff
Andrij-Latkivskyj <latkivskijandrij@gmail.com>
2023-05-04 17:16:24 +0300

    Update dz_9.py

--- dz_9.py
+++ dz_9.py
@@ -1,5 +1,4 @@
-USERS = {}
-
+CONTACTS = {}
 
 def input_error(func):
     def inner(*args):
@@ -8,94 +7,89 @@ def input_error(func):
             return result
         except KeyError:
             return "No user"
-        except ValueError:
+        except TypeError:
             return "Give me name and phone please"
+        except ValueError:
+            return "Give me name and phone please"   
         except IndexError:
             return "Enter user name"
     return inner
 
 
-def hello_user(_):
+def hello_user():
     return "How can I help you?"
 
 
-def exit(_):
-    print('Good bye!')
-    return
-
-def unknown_command(_):
-    return "unknown command"
+@input_error
+def add_contact(name, phone):
+    CONTACTS[name] = phone
+    return f'User {name} added contact {phone}'
 
 @input_error
-def add_user(args):
-    name, phone = args
-    USERS[name] = phone
-    return f"User {name} added!"
+def change_phone(name, phone):
+    CONTACTS[name] = phone
+    return f'{name} have a new phone: {phone}'
 
 @input_error
-def change_phone(args):
-    name, phone = args
-    old_phone = USERS[name]
-    USERS[name] = phone
-    return f"У {name} тепер телефон: {phone} Старий номер: {old_phone}"
+def show_phone(name):
+    return f'User {name} showing phone {CONTACTS[name]}'
+
+
+def show():
     
-
-def show_all(_):
     result = ''
-    for name, phone in USERS.items():
-        result += f'name: {name} phone: {phone}\n'
+    for name, phone in CONTACTS.items():
+        result += f'\n name: {name}  phone: {phone}'
+    
     return result
 
 
-def show_phone(args):
-    name = args
-    
-    phone_to_show = str(USERS[name[0]])
-    
-    return f'name: {(name[0]).title()} phone: {phone_to_show}'
+def unknown_command(command):
+    return f'Not command {command}'
 
-HANDLERS = {
+
+
+exit_command = ['good bye', 'close', 'exit']
+
+
+commands = {
     'hello': hello_user,
-    'add': add_user,
+    'add': add_contact,
     'change': change_phone,
-    'show all': show_all,
-    'exit': exit,
-    'good bye': exit,
-    'close': exit,
     'phone': show_phone,
-
-}
-
-
-def parse_input(user_input):
-    command, *args = user_input.split()
-    command = command.lstrip()
-
-    try:
-        handler  = HANDLERS[command.lower()]
-    except KeyError:
-        if args:
-            command  = command + " " + args[0]
-            args = args[1:]
-        handler = HANDLERS.get(command.lower(), unknown_command)
-    return handler, args
+    'show_all': show,
+    
+    }
 
 
 
 def main():
+
     while True:
-        user_input = input('Please enter command and args: ')
-        handler, *args = parse_input(user_input)
-        result = handler(*args)
-        if not result:
-           
+
+        command, *data = input('Please enter [name] , [phone] :  ').strip().split(' ', 1)
+
+        
+        if command in exit_command:
+            print('Good bye!')
             break
+
+        elif commands.get(command):
+            handler = commands.get(command)
+            if data:
+                data = data[0].split(',')
+
+            result = handler(*data)
+        
+
+        else:
+            result = unknown_command(command)
+
 
         print(result)
 
 
+
+
 if __name__ == '__main__':
     main()
-
-
-
